e4ea34ef62974b7d82129cc752a71bb160c3d3dd
Jos Boumans <kane@cpan.org>
2006-11-08 13:29:01 +0000

    * unf*ckup the repo
    git-svn-id: http://oss.dwim.org/term-ui@1666 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- t/inc/conf.pl
+++
@@ -1,7 +0,0 @@
-### so T::RL doesn't go nuts over no console
-BEGIN{ $ENV{LINES} = 25; $ENV{COLUMNS} = 80; }
-
-### perl core gets upset if we print stuff to STDOUT...
-close STDOUT if $ENV{PERL_CORE};
-
-1;
