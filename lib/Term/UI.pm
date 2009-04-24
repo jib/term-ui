af5e3ee7454276997107e99dc56ef5f9876e5eee
Jos Boumans <kane@cpan.org>
2009-04-24 13:57:55 +0000

    r7257@myriad-wifi:  kane | 2009-04-24 15:57:50 +0200  * address: #45264 Patch for Term::UI; when get_reply is called without a    default, term::ui would emit a warning
    git-svn-id: http://oss.dwim.org/term-ui@2589 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/Term/UI.pm
+++ lib/Term/UI.pm
@@ -147,7 +147,7 @@ sub get_reply {
             
             ### so this choice is the default? add it to 'prompt_add'
             ### so we can construct a "foo? [DIGIT]" type prompt
-            $prompt_add = $i if $choice eq $args->{default};
+            $prompt_add = $i if (defined $args->{default} and $choice eq $args->{default});
 
             ### create a "DIGIT> choice" type line
             $args->{print_me} .= sprintf "\n%3s> %-s", $i, $choice;
